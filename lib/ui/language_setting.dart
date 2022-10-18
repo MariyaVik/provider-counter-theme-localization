871adef69e3acf2f87dbb2aa8b784f783e59af28
Mariya Pelageicheva <pelagei4eva@gmail.com>
2022-10-19 00:09:50 +0500

    ListTile of languages

--- lib/ui/language_setting.dart
+++ lib/ui/language_setting.dart
@@ -2,30 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:provider_simple_counter/common/locale_model.dart';
 import 'package:provider_simple_counter/common/localization.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LanguageScreen extends StatelessWidget {
   const LanguageScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final languagesList = [];
-
     return Scaffold(
       appBar: AppBar(),
-      body: ListView.builder(
+      body: ListView.separated(
         itemBuilder: (context, index) {
+          var locale = Localization.supportedLocales[index];
           return ListTile(
-            title: Text('${Localization.supportedLocales[index]}'),
+            title: Text(lookupAppLocalizations(locale).language),
+            subtitle: Text(lookupAppLocalizations(locale).language_en),
             onTap: () {
-              context
-                  .read<LocaleModel>()
-                  .changeLocale(Localization.supportedLocales[index]);
+              context.read<LocaleModel>().changeLocale(locale);
             },
-            selected: context.watch<LocaleModel>().currentLocale ==
-                Localization.supportedLocales[index],
+            selected: context.watch<LocaleModel>().currentLocale == locale,
           );
         },
         itemCount: Localization.supportedLocales.length,
+        separatorBuilder: (BuildContext context, int index) => const Divider(),
       ),
     );
   }
